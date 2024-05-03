10dad9507a15b3d3389d06dbc268369df8246c0b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-02 22:14:17 -0300

    02/05

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log(window.navigator.platform);
 console.log(window.navigator.userAgent);
 
 console.log(window.screen.width);
-console.log(window.screen.height); */
+console.log(window.screen.height); 
 
 console.log(document);
 console.log(document.head);
@@ -26,7 +26,41 @@ console.log(lista.lastElementChild);
 console.log(lista.parentElement);
 
 console.log(document.getElementById("listinha"));
-console.log(document.getElementById("Item01"));
+console.log(document.getElementById("Item01"));*/
+
+//SEGUNDA PARTE DA AULA
+
 
 console.log(document.querySelectorAll(".ordenada"));
-console.log(document.querySelectorAll("#Item01")); 
+console.log(document.querySelector(".ordenada")); 
+
+console.log(document.querySelector(".ordenada").matches(".ordenada"));
+
+let lista = document.querySelector(".ordenada");
+
+console.log(lista.matches("#listinha"));
+
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByClassName("ordenada"));
+
+let titulo = document.getElementById("titulo");
+titulo.innerText = "<i>Mudei</i>";
+
+let logo = document.getElementsByTagName("img") [0];
+logo.setAttribute("alt", "Logo da ETEAB");
+logo.removeAttribute("alt");
+console.log(logo.getAttribute("alt"));
+
+titulo.classList.add("vermelho");
+titulo.classList.remove("vermelho");
+
+titulo.classList.add("roxo");
+titulo.classList.contains("roxo");
+
+titulo.classList.toggle("negrito");
+
+let paragrafo = document.getElementById("paragrafo01");
+paragrafo.style.fontSize = "20px";
+paragrafo.style.color = "green";
+paragrafo.style.marginLeft = "100px";
+
